f9c70892b8dd9222daf91ea9111089d90146fe7c
James <james.kelada@auspost.com.au>
2023-04-29 01:23:13 +1000

    eslinted v2

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,5 +1,5 @@
 // components/Comments/Comments.jsx
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 const Comments = () => {
   // Fetch and handle comments here
--- src/components/PageSection/PageSection.jsx
+++ src/components/PageSection/PageSection.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './PageSection.css';
 
-const PageSection = ({ title, children, backgroundColor }) => {
+// eslint-disable-next-line react/prop-types
+const PageSection = ({title, children, backgroundColor}) => {
   return (
-    <section className="page-section" style={{ backgroundColor }}>
+    <section className="page-section" style={{backgroundColor}}>
       {title && <h2 className="section-title">{title}</h2>}
       {children}
     </section>
