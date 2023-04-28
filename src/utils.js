0397c3cade723d5ee5d50452374586aa22913f17
James <james.kelada@auspost.com.au>
2023-04-29 01:29:03 +1000

    eslinted super

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
-import { doc, updateDoc, increment } from 'firebase/firestore';
-import { firestore } from './firebase'; // Adjust the import path accordingly
+import {doc, updateDoc, increment} from 'firebase/firestore';
+import {firestore} from './firebase'; // Adjust the import path accordingly
 
 export const incrementViewCount = async (videoId) => {
   const videoRef = doc(firestore, 'videos', videoId);
-  await updateDoc(videoRef, { viewCount: increment(1) });
+  await updateDoc(videoRef, {viewCount: increment(1)});
 };
